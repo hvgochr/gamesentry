b9a016ccaa66de0dca6129641a8408e30aa2d39d
Hugo Ch <charrade.hugo@gmail.com>
2026-03-25 18:25:31 +0100

    Update app-logo component

--- services/web/resources/js/components/app-logo.tsx
+++ services/web/resources/js/components/app-logo.tsx
@@ -6,9 +6,10 @@ export default function AppLogo() {
             <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
                 <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
             </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Laravel Starter Kit
+            <div className="ml-1 grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-semibold">Gamesentry</span>
+                <span className="truncate text-xs text-muted-foreground">
+                    Bot dashboard
                 </span>
             </div>
         </>
