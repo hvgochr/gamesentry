b3ba6af460eb00f83a48ad2fdc91f3b8d8896de9
Hugo Ch <charrade.hugo@gmail.com>
2026-03-25 18:03:33 +0100

    Define discordServers & trackedPlayers relationships to User model

--- services/web/app/Models/User.php
+++ services/web/app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -48,5 +50,15 @@ class User extends Authenticatable
             'password' => 'hashed',
             'two_factor_confirmed_at' => 'datetime',
         ];
+    }
+
+    public function discordServers(): HasMany
+    {
+        return $this->hasMany(DiscordServer::class);
+    }
+
+    public function trackedPlayers(): HasManyThrough
+    {
+        return $this->hasManyThrough(TrackedPlayer::class, DiscordServer::class);
     }
 }
