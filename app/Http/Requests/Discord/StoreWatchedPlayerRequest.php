7663782adc18605fad6e6bb97be5b1f7ecf182be
Hugo Ch <charrade.hugo@gmail.com>
2026-04-09 18:39:16 +0200

    Create store & update WatchedPlayer requests

---
+++ app/Http/Requests/Discord/StoreWatchedPlayerRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests\Discord;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreWatchedPlayerRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return $this->user()->can('update', $this->route('discordServer'));
+    }
+
+    public function rules(): array
+    {
+        return [
+            'game' => ['required', Rule::in(['lol', 'tft'])],
+            'routing_region' => ['required', Rule::in(['americas', 'asia', 'europe', 'sea'])],
+            'game_name' => ['required', 'string', 'max:64'],
+            'tag_line' => ['required', 'string', 'max:16'],
+            'discord_user_id' => ['required', 'string', 'max:32'],
+            'is_active' => ['nullable', 'boolean'],
+        ];
+    }
+}
