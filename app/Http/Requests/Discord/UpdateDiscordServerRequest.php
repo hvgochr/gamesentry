63057548555135445b6d3281c058a93e2beeaad7
Hugo Ch <charrade.hugo@gmail.com>
2026-04-09 18:06:23 +0200

    Create UpdateDiscordServer request

---
+++ app/Http/Requests/Discord/UpdateDiscordServerRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests\Discord;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateDiscordServerRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return $this->user()->can('update', $this->route('discordServer'));
+    }
+
+    public function rules(): array
+    {
+        return [
+            'discord_channel_id' => ['required', 'string', 'max:32'],
+        ];
+    }
+}
