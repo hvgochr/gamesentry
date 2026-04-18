b4bab1e9029ad61cfb84eaa3d7ccb444cb3f992f
Hugo Ch <charrade.hugo@gmail.com>
2026-04-19 00:22:14 +0200

    Remove unnecessary declarations

--- app/Services/Discord/DiscordService.php
+++ app/Services/Discord/DiscordService.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Services\Discord;
 
 use App\Services\Discord\Exceptions\DiscordDuplicateNonceException;
--- app/Services/Groq/GroqService.php
+++ app/Services/Groq/GroqService.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Services\Groq;
 
 use Illuminate\Http\Client\PendingRequest;
