d28fc4fa283143238555add856db980c30b54320
Hugo Ch <charrade.hugo@gmail.com>
2026-06-22 18:04:03 +0200

    Align Inertia UI copy with existing Livewire translations

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -56,7 +56,7 @@ export default function Login({ status, canResetPassword }: Props) {
                                             className="ml-auto text-sm"
                                             tabIndex={5}
                                         >
-                                            Forgot password?
+                                            Forgot your password?
                                         </TextLink>
                                     )}
                                 </div>
